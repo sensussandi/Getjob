6360268fc73422eb9344a79b9837d72178381660
sensussandi <sensussandi74@gmail.com>
2025-10-31 16:51:08 +0700

    update dashboardMHS page

--- src/app/dashboardMHS/page.js
+++ src/app/dashboardMHS/page.js
@@ -38,7 +38,7 @@ export default function DashboardMHS() {
         }}
         className="mt-6 bg-red-900 text-white px-6 py-2 rounded-lg hover:bg-red-800 transition"
       >
-        Logout.
+        Logout
       </button>
   </div>
 );
